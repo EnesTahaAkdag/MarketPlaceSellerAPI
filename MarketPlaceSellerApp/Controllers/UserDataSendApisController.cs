3758a5d6a4a50235795bf8c6bd3b86fa97b81c30
EnesTahaAkdag <akdagenestaha@gmail.com>
2024-10-22 16:01:49 +0300

    A

--- MarketPlaceSellerApp/Controllers/UserDataSendApisController.cs
+++ MarketPlaceSellerApp/Controllers/UserDataSendApisController.cs
@@ -41,7 +41,7 @@ namespace MarketPlaceSellerApp.Controllers
 						u.Age,
 						ProfileImageBase64 = string.IsNullOrEmpty(u.ProfileImage)
 							? null
-							: $"https://de29-37-130-115-91.ngrok-free.app/profile_images/{u.ProfileImage}"
+							: $"https://45f9-37-130-115-91.ngrok-free.app/profile_images/{u.ProfileImage}"
 					})
 					.FirstOrDefaultAsync();
 
